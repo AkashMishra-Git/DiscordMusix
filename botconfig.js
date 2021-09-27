78cbd7bc5740c5b69f75b11f6b023a4ff54a75b9
As4lto <79124977+FIREBLADEGAMING@users.noreply.github.com>
2021-09-27 17:06:58 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //Admins of the bot
+  Admins: ["668818456673779737"], //Admins of the bot
   ExpressServer: true,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  SupportServer: "https://discord.gg/SchEbTFaNM", //Support Server Link
+  Token: process.env.Token || "ODEyOTM5Mzk1ODY2MDM0MjA4.YDICzw.fIqBquG5G5vyqfsTfN7d3NFvlmk", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "812939395866034208", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "95B0jnDA3-TGIZLgp0TdEjRHoEyN7yDw", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "ampm.15dec", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
@@ -20,16 +20,16 @@ module.exports = {
   //Lavalink
    Lavalink: {
     id: "Main",
-    host: "host name or IP",
-    port: port,
-    pass: "your password", 
-    secure: false, // Set this to true if you're self-hosting lavalink on replit.
+    host: "lava.link",
+    port: 80,
+    pass: "youshallnotpass",
+    // Set this to true if you're self-hosting lavalink on replit.
   },
 
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "7fc38735b1e4410cbdc43cc5278bfd33", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "92c8375bb9eb446ba7c636d39771fbe8", //Spotify Client Secret
   },
 };
